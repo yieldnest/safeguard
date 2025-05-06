64b16924d462c4f15483345627bd61e15c8acf8b
danoctavian <danoctavian91@gmail.com>
2025-05-06 20:52:01 +0300

    add wip transaction checker

--- src/SafeGuard.sol
+++ src/SafeGuard.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.0;
 import {BaseTransactionGuard, ITransactionGuard} from "lib/safe-smart-account/contracts/base/GuardManager.sol";
 import {BaseModuleGuard, IModuleGuard} from "lib/safe-smart-account/contracts/base/ModuleManager.sol";
 import {IERC165} from "lib/safe-smart-account/contracts/interfaces/IERC165.sol";
+import {Enum} from "lib/safe-smart-account/contracts/libraries/Enum.sol";
+import {ISafe} from "lib/safe-smart-account/contracts/interfaces/ISafe.sol";
 
-
-abstract contract SafeGuard is BaseTransactionGuard, BaseModuleGuard {
+contract SafeGuard is BaseTransactionGuard, BaseModuleGuard {
 
     /**
      * @inheritdoc IERC165
@@ -17,4 +18,56 @@ abstract contract SafeGuard is BaseTransactionGuard, BaseModuleGuard {
             interfaceId == type(IModuleGuard).interfaceId || // 0x58401ed8
             interfaceId == type(IERC165).interfaceId; // 0x01ffc9a7
     }
+
+
+    /**
+     * @notice Called by the Safe contract before a transaction is executed.
+     * @dev Reverts if the transaction is not executed by an owner.
+     * @param msgSender Executor of the transaction.
+     */
+    function checkTransaction(
+        address,
+        uint256,
+        bytes memory,
+        Enum.Operation,
+        uint256,
+        uint256,
+        uint256,
+        address,
+        // solhint-disable-next-line no-unused-vars
+        address payable,
+        bytes memory,
+        address msgSender
+    ) external view override {
+        require(ISafe(msg.sender).isOwner(msgSender), "msg sender is not allowed to exec");
+    }
+
+    /**
+     * @inheritdoc ITransactionGuard
+     */
+    function checkAfterExecution(bytes32, bool) external pure override {
+        // No-op implementation
+    }
+
+    /**
+     * @inheritdoc IModuleGuard
+     */
+    function checkModuleTransaction(
+        address,
+        uint256,
+        bytes memory,
+        Enum.Operation,
+        address
+    ) external pure override returns (bytes32 moduleTxHash) {
+        // No-op implementation
+        return bytes32(0);
+    }
+
+    /**
+     * @inheritdoc IModuleGuard
+     */
+    function checkAfterModuleExecution(bytes32, bool) external pure override {
+        // No-op implementation
+    }
+
 }
