e72ac3d481a3034fd99c8ad74ade2ea12972c1e5
danoctavian <danoctavian91@gmail.com>
2025-05-07 19:34:03 +0300

    add test for inactive rule

--- test/SafeGuard.t.sol
+++ test/SafeGuard.t.sol
@@ -10,6 +10,8 @@ import {IVault} from "lib/yieldnest-vault/src/library/VaultLib.sol";
 import {SafeRules} from "lib/yieldnest-vault/script/rules/SafeRules.sol";
 import {BaseRules} from "lib/yieldnest-vault/script/rules/BaseRules.sol";
 import {IValidator} from "lib/yieldnest-vault/src/interface/IValidator.sol";
+import {Enum} from "lib/safe-smart-account/contracts/libraries/Enum.sol";
+import {Guard} from "lib/yieldnest-vault/src/module/Guard.sol";
 
 contract SafeGuardTest is Test {
     SafeGuard implementation;
@@ -73,5 +75,52 @@ contract SafeGuardTest is Test {
         assertEq(retrievedRule1.isActive, true);
         assertEq(address(retrievedRule1.validator), address(0));
         assertEq(retrievedRule1.paramRules.length, 2);
+    }
+
+    function test_checkTransaction_revertsOnInactiveRule() public {
+        // Create sample targets
+        address mockContract = address(0x1234);
+        address mockToken = address(0x5678);
+
+        // Use BaseRules to get predefined rules
+        SafeRules.RuleParams memory approvalRule = BaseRules.getApprovalRule(mockToken, address(this));
+        SafeRules.RuleParams memory depositRule = BaseRules.getDepositRule(mockContract, user);
+
+        // Set up rules - but make depositRule inactive
+        depositRule.rule.isActive = false;
+
+        SafeRules.RuleParams[] memory ruleParams = new SafeRules.RuleParams[](2);
+        ruleParams[0] = approvalRule;
+        ruleParams[1] = depositRule;
+
+        vm.startPrank(processorManager);
+        SafeRules.setProcessorRules(IVault(address(safeguard)), ruleParams, true);
+        vm.stopPrank();
+
+        // Verify the rules were set correctly
+        IVault.FunctionRule memory retrievedRule =
+            safeguard.getProcessorRule(depositRule.contractAddress, depositRule.funcSig);
+        assertEq(retrievedRule.isActive, false);
+
+        // Create transaction data for the inactive rule (deposit function)
+        bytes memory txData = abi.encodeWithSelector(depositRule.funcSig, 1000, user);
+
+        // Expect revert when checking transaction with inactive rule
+        vm.expectRevert(
+            abi.encodeWithSelector(Guard.RuleNotActive.selector, depositRule.contractAddress, depositRule.funcSig)
+        );
+        safeguard.checkTransaction(
+            depositRule.contractAddress, // to
+            0, // value
+            txData, // data
+            Enum.Operation.Call, // operation
+            0, // safeTxGas
+            0, // baseGas
+            0, // gasPrice
+            address(0), // gasToken
+            payable(address(0)), // refundReceiver
+            bytes(""), // signatures
+            address(this) // executor
+        );
     }
 }
